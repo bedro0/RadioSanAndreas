a9dc46cc7dbd60a5c16ea869571f5989838b2019
Badri Isiani <badri.isiani@gmail.com>
2024-07-27 23:24:27 -0400

    Frontend tweaks
    [+] added a custom background when each radio station is playing
    
    [~] fixed the alignment of radio buttons

--- frontend/sveltekit-src/src/routes/+page.svelte
+++ frontend/sveltekit-src/src/routes/+page.svelte
@@ -4,42 +4,65 @@
 
 <script lang="ts">
     import {tick} from 'svelte';
+    let playerVolume = 1;
+    let playerObj;
     let audio_playing = false;
     let title="Grove Street Radio";
     let favicon="gsr.png";
     let all_stations=["bouncefm", "csr", "kdst", "kjah", "krose", "mastersounds", "playbackfm", "radiols", "radiox", "sfur"];
+    let activeStates = Array(all_stations.length).fill(false);
     let main_src, alt_src;
+    let hover=false;
+    let station_playing;
+
+
     async function playAudio(station){
         audio_playing = false;
         await tick();
         main_src = `https://icecast.loonartech.net/${station}.ogg?nocache=${new Date().getTime()}`;
         alt_src = `https://icecast.loonartech.net/${station}.mp3?nocache=${new Date().getTime()}`
         audio_playing = true;
+        station_playing = station;
     }
+    function setHover(index, state) {
+		activeStates = activeStates.map((active, i) => i === index ? state : hover);
+	}
 </script>
 
 <style lang="scss">
     @import "../main.scss";
 </style>
+{#if !audio_playing}
+    <div id="stations">
+        {#each all_stations as station, index}
+        <script lang="ts">
 
-<div id="stations">
-    {#each all_stations as station}
-    <script lang="ts">
+        </script>
+        <button 
+        class:hover={activeStates[index]}
+        on:mouseenter={() => setHover(index, true)} 
+        on:mouseleave={() => setHover(index, false)} 
+        on:click={() => playAudio(station)}
+        >
+            <img src="/src/visual-assets/logos/{station}.png" alt={station}>
 
-    </script>
-    <button on:click={() => playAudio(station)}>
-        <img src="/src/visual-assets/{station}.png" alt={station}>
-
-    </button>
-    {/each}
-</div>
-
-<button on:click={() => audio_playing=false}>Pause</button>
+        </button>
+        {/each}
+    </div>
+{/if}
+<!--
+<button id="stop" on:click={() => playerObj.play()}>Play</button>
+<button id="stop" on:click={() => playerObj.pause()}>Pause</button>
+-->
 
 {#if audio_playing}
-    <audio autoplay>
-        <source bind:this="{main_src}" type="audio/ogg"/>
+    <img id=background-image src="src/visual-assets/backgrounds/{station_playing}.jpg" alt={station_playing}>
+    <audio autoplay bind:this={playerObj} bind:volume="{playerVolume}">
+        <source src="{main_src}" type="audio/ogg"/>
         <source src="{alt_src}" type="audio/mpeg"/>
         "Audio not supported on this browser"
     </audio>
+    <div id=controls>
+        <input type="range" id=volume min="0" max="1" step="0.01" bind:value={playerVolume}>
+    </div>
 {/if}
