d3ea1624b7d6e69bab266eceb5e6c6edc96977cd
Badri Isiani <badri.isiani@gmail.com>
2024-10-15 22:02:00 -0400

    [+] Added separate definition for wctr route, which conforms to quirks of that specific radio station.

--- sveltekit/src/routes/[radioStation]/+page.server.js
+++ sveltekit/src/routes/[radioStation]/+page.server.js
@@ -2,6 +2,6 @@ import fs from "fs";
 
 export function load(){
 
-    const enabledStations = fs.readFileSync("/radiosa/sveltekit/src/enabled-stations.json", "utf8");
+    const enabledStations = fs.readFileSync("src/enabled-stations.json", "utf8");
     return {"enabledStations": enabledStations}
 }
