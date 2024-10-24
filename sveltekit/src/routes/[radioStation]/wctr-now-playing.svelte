d42cde05766c6932f6698a382c04e1ce49ec1a1e
bedro0 <ray@loonartech.net>
2024-10-23 22:55:02 -0400

    [+] Cleaner UI elements

--- sveltekit/src/routes/[radioStation]/wctr-now-playing.svelte
+++ sveltekit/src/routes/[radioStation]/wctr-now-playing.svelte
@@ -5,8 +5,8 @@
 <div class="now-playing">
     {#if ["(Intro)", "(Mid)", "(Outro)"].some(suffix => nowPlaying.path.includes(suffix))}
         <h3>NOW PLAYING</h3>
-        <p> {nowPlaying.artist} </p>
-        <p> {nowPlaying.title} </p>
+        <div class="playing-artist"><div id="artist"><span/></div> &emsp; {nowPlaying.artist}</div>
+        <div class="playing-track"><div id="title"><span/></div> &emsp; {nowPlaying.title}</div>
     {:else}
         <h3>BREAK</h3>
     {/if}
